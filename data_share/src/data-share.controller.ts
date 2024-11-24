d4f2bb5647f85f9a593df4bf8d8d3a8d63e87a5a
Duy-Vu03-02 <duyvu11122002@gmail.com>
2024-11-24 17:43:40 +0700

    ko decode duoc kho chiu that su

--- data_share/src/data-share.controller.ts
+++ data_share/src/data-share.controller.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import { HospitalModel } from './hospital';
 import crypto from 'crypto';
 import { Web3Service } from './web3';
+import zlib from 'zlib';
 
 export class DataShareController {
     public static shareData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -14,14 +15,11 @@ export class DataShareController {
                     if (hospital) {
                         const publicKey = hospital.publicKey;
                         const base64_data = JSON.stringify(patient_data);
-                        const encode = DataShareController.encryptData(publicKey, base64_data);
+                        const encode = DataShareController.encryptAndCompress(base64_data, publicKey);
 
                         const accounts = await Web3Service.getAccounts();
                         const account = accounts[0];
-                        const transaction = Web3Service.getConstract().methods.shareData(
-                            patient_data.cccd,
-                            Buffer.from(encode),
-                        );
+                        const transaction = Web3Service.getConstract().methods.shareData(patient_data.cccd, encode);
 
                         const gas = await transaction.estimateGas({ from: account });
 
@@ -51,7 +49,6 @@ export class DataShareController {
 
             throw new Error('Khong the chia se data');
         } catch (err) {
-            console.error(err);
             res.json({
                 message: 'That bai khoong the send data',
             });
@@ -60,20 +57,73 @@ export class DataShareController {
         }
     };
 
-    public static encryptData(publicKey, data) {
+    private static encryptAndCompress(data, publicKey: string) {
+        const compressedData = zlib.gzipSync(data);
+        const encryptedData = crypto.publicEncrypt(publicKey, Buffer.from(compressedData));
+        console.log(encryptedData);
+        return encryptedData;
+    }
+    private static decryptAndDecompress(encryptedData, privateKey: string) {
         try {
-            const encryptedData = crypto.publicEncrypt(publicKey, Buffer.from(data));
-            return encryptedData.toString('base64');
+            console.log(encryptedData);
+            const decryptedData = crypto.privateDecrypt(privateKey, encryptedData);
+            const decompressedData = zlib.gunzipSync(decryptedData).toString('utf-8');
+            return decompressedData;
         } catch (err) {
-            console.log(err);
+            console.error(err);
         }
     }
 
-    public static decryptData(privateKey, encryptedData) {
-        const bufferEncryptedData = Buffer.from(encryptedData, 'base64');
-        const decrypted = crypto.privateDecrypt(privateKey, bufferEncryptedData);
-        return decrypted.toString();
-    }
+    public static receiveData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { patient_cccd } = req.body;
+            if (req.user && patient_cccd) {
+                const hospital = await HospitalModel.findById(req.user.hospital_id);
 
-    public static receiveData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {};
+                if (hospital) {
+                    const privateKey = hospital.privateKey;
+                    if (privateKey) {
+                        const accounts = await Web3Service.getAccounts();
+                        const account = accounts[0];
+                        const data = await Web3Service.getConstract()
+                            .methods.getData(patient_cccd)
+                            .call({ from: account });
+
+                        if (!data || data === '0x') {
+                            throw new Error('Khong ton tai du lieu nay');
+                        }
+
+                        const buffreBase64 = Buffer.from(data.slice(2), 'hex');
+                        const decryptedData = DataShareController.decryptAndDecompress(
+                            buffreBase64,
+                            privateKey.toString(),
+                        );
+                        // console.log(buffreBase64);
+                        // const decryptedData = crypto.privateDecrypt(privateKey.toString(), buffreBase64);
+                        // const decompressedData = zlib.gunzipSync(decryptedData).toString('utf-8');
+                        // console.log(decryptedData);
+
+                        // console.log(decompressedData);
+
+                        res.json({
+                            messgae: 'Da tim thay du lieu',
+                            data: data,
+                        });
+                        res.status(200);
+                        res.end();
+                        return;
+                    }
+                }
+            }
+
+            throw new Error('Khong tha nan data');
+        } catch (err) {
+            console.log(err);
+            res.json({
+                message: 'That bai khoong the receive data',
+            });
+            res.status(404);
+            res.end();
+        }
+    };
 }
--- data_share/src/index.ts
+++ data_share/src/index.ts
@@ -12,3 +12,66 @@ app.use(router);
 app.listen(PORT, () => {
     console.log('Service SHARE DATA start SUCCESS!!!');
 });
+
+// import crypto from 'crypto';
+// import zlib from 'zlib';
+
+// const cccd = '019203001503';
+
+// const generateMd5Buffer = (inputString: string): Buffer => {
+//     const hash = crypto.createHash('md5');
+//     hash.update(inputString);
+//     return hash.digest();
+// };
+
+// const generateSha256Buffer = (inputString: string): Buffer => {
+//     const hash = crypto.createHash('sha256');
+//     hash.update(inputString);
+//     return hash.digest();
+// };
+
+// // Sinh cặp khóa RSA duy nhất (public và private)
+// const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+//     modulusLength: 2048, // Độ dài khóa RSA
+//     publicKeyEncoding: { type: 'spki', format: 'pem' },
+//     privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+// });
+
+// /**
+//  * Hàm mã hóa và nén dữ liệu
+//  * @param {string} data - Dữ liệu cần mã hóa và nén
+//  * @param {string} publicKey - Khóa công khai RSA để mã hóa
+//  * @returns {string} - Dữ liệu mã hóa đã nén dưới dạng Base64
+//  */
+// function encryptAndCompress(data, publicKey): Buffer {
+//     const compressedData = zlib.gzipSync(data);
+//     const encryptedData = crypto.publicEncrypt(publicKey, compressedData);
+//     return encryptedData;
+// }
+
+// function decryptAndDecompress(encryptedData, privateKey) {
+//     console.log(encryptedData);
+//     console.log('====');
+//     console.log(Buffer.from(encryptedData, 'base64'));
+//     const decryptedData = crypto.privateDecrypt(privateKey, Buffer.from(encryptedData, 'base64'));
+//     const decompressedData = zlib.gunzipSync(decryptedData).toString('utf-8');
+//     return decompressedData;
+// }
+
+// // Dữ liệu gốc
+// const originalData = JSON.stringify({
+//     'chup-x-quang': 'ko ls',
+//     'sieu-am': 'ko ls',
+//     'sieu con cac': 'hahaha',
+//     cccd: '1212',
+// });
+
+// // Mã hóa và nén dữ liệu
+// const encryptedData = encryptAndCompress(originalData, publicKey);
+
+// // Giải mã và giải nén dữ liệu
+// const decryptedData = decryptAndDecompress(encryptedData, privateKey);
+// console.log('Decrypted and Decompressed Data:', decryptedData);
+
+// // Kiểm tra tính toàn vẹn của dữ liệu
+// console.log('Original Data Matches Decrypted Data:', originalData === decryptedData);
